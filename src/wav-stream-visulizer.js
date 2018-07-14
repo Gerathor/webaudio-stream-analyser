82a4289c90d600a59b59870bb47e0ce64d628f50
sutaking <zhaofeng.vip@gmail.com>
2018-07-14 17:31:56 +0800

    1

--- src/wav-stream-visulizer.js
+++ src/wav-stream-visulizer.js
@@ -1,7 +1,31 @@
 
 
 const StreamVisulizer = () => {
+    
+    let context;
+    
+    let hasCanceled = false;
 
+    const feed = buf => {
+        // create audio context
+
+        // scheulde buffer
+
+        // decode
+    };
+
+    return {
+        feed : buf => feed(buf),
+        stop : () => {
+            hasCanceled = true;
+            if (context) {
+                context.close();
+            }
+        },
+        recorder : () => recorder(),
+        saveWav : () => saveWav()        
+    }
 };
 
 export default StreamVisulizer;
+
